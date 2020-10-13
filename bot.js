7c3de85d7efff29fe1f046edc2349b0a8c505235
icannotunsee <66531908+icannotunsee@users.noreply.github.com>
2020-10-13 15:49:08 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,10 +21,10 @@ client.on('ready', () => {
      if(message.content.toLowerCase().startsWith(PREFIX + 'slowmode')) {
          var str = message.content.split(prefix + 'slowmode ');
          var num = parseInt(str[1], 10);
-         message.channel.setRateLimitPerUser(num);
+         message.channel.setRateLimitPerUser(num).then(() => {
          message.channel.send(`slowmode set to ${num}s.`);
-    }
   });
+     }
 }
 
 
