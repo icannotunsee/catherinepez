c7f8a138951a0064734f3118b272843f18f82a17
icannotunsee <66531908+icannotunsee@users.noreply.github.com>
2020-10-13 17:08:34 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,14 +16,5 @@ client.on('ready', () => {
     }
   });
 
-  client.on('message', message => {
-     const args = message.content.split(" ").slice(1);
-     if(message.content.toLowerCase().startsWith(PREFIX + 'slowmode')) {
-         var str = message.content.split(prefix + 'slowmode ');
-         var num = parseInt(str[1], 10);
-         message.channel.setRateLimitPerUser(num).then(() => {
-         message.channel.send(`slowmode set to ${num}s.`);
-    })};
-  }
 
   client.login(process.env.BOT_TOKEN);
