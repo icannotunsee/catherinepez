fd46c1deed0cca188cac1aeba6ed6920a05fca05
icannotunsee <66531908+icannotunsee@users.noreply.github.com>
2021-01-04 23:36:27 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,35 +3,16 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity('unsee content', { type: 'WATCHING' });
+    client.user.setActivity('girls kingdom', { type: 'WATCHING' });
 });
 
 
   client.on('message', message => {
     const args = message.content.split(" ").slice(1);
-    if(message.content.startsWith('c!say') && message.guild.member(message.author).hasPermission("MANAGE_CHANNELS")) {
+    if(message.content.startsWith('d!say') && message.guild.member(message.author).hasPermission("MANAGE_CHANNELS")) {
         message.delete()
         var saytext = args.join(" ");
         message.channel.send(saytext);
-    }
-  });
-
-client.on('message', msg => {
-    if (msg.content === 'mod abuse') {
-      msg.reply('use <#751881836455657533> to report a mod.');
-    }
-  });
-
-client.on('message', msg => {
-    if (msg.content === 'admin abuse') {
-      msg.reply('use <#751881836455657533> to report an admin.');
-    }
-  });
-
-
-client.on('message', msg => {
-    if (msg.content === 'staff abuse') {
-      msg.reply('use <#751881836455657533> to report staff.');
     }
   });
 
