b98b604e602b21c9c4f3d8f6429d65ce3a3ab2db
icannotunsee <66531908+icannotunsee@users.noreply.github.com>
2021-01-05 17:11:58 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,5 +35,30 @@ client.on("message", message => {
     }
   });
 
+async function quote(msg) {  
+   // Split the message into arguments
+   let content = msg.content.split(' ');  
+   // Check if message id is supplied
+   if (content[1] == undefined) {  
+      msg.reply('You need to suplly a message id');  
+      return;  
+   }  
+   // Retrieve the message
+   let quotedMessage = await msg.channel.fetchMessage(content[1]);  
+   // Parse the timestamp 
+   let date = new Date(quotedMessage.createdTimestamp);  
+   // Send the quote  
+   channel = client.channels.cache.get('780622004968620033');
+   msg.channel.send(`"${quotedMessage.content}" - ${quotedMessage.author}, ${date.getFullYear()}`);
+  }  
+    
+ client.on('message', message => {
+    const args = message.content.split(" ").slice(1);
+    if(message.content.toLowerCase == "nigga" || message.content.toLowerCase == "dyke") {
+        message.delete()
+        message.channel.reply(saytext);
+        quote(message);
+    }
+  });
 
   client.login(process.env.BOT_TOKEN);
