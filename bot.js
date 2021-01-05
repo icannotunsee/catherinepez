c3b50b26e164b8351899b44507034bd057e8df25
icannotunsee <66531908+icannotunsee@users.noreply.github.com>
2021-01-05 09:28:15 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,4 +16,13 @@ client.on('ready', () => {
     }
   });
 
+client.on('message', message => {
+  const args = message.content.split(" ").slice(1);
+  if(message.content.startsWith('d!announce') && message.guild.member(message.author).hasPermission("MANAGE_CHANNELS")) {
+      message.delete()
+      var saytext = args.join(" ");
+      client.channels.get(<#780917089066025020>).send(saytext);
+    }
+  });
+
   client.login(process.env.BOT_TOKEN);
