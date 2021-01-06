b4bd169b797d030dc465606ea874ae378ffd9cfc
icannotunsee <66531908+icannotunsee@users.noreply.github.com>
2021-01-06 16:14:24 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,11 @@ client.on('ready', () => {
     client.user.setActivity('girls kingdom', { type: 'WATCHING' });
 });
 
-client.on('messageReactionAdd', async (reaction, user) => {
-    if (reaction.id === '<a:GK_rotatingcrown:780890745678463046>' && message.channel.name === 'sign') {
-        channel = client.channels.cache.get('780601741614776330');
-        message.member.channel.reply("welcome to the server!");
+client.on('messageReactionAdd', (reaction, user) => {
+    if(user.id !== bot.user.id) {
+        if(reaction.message.id === '780887860554760201') {
+            user.send("thanks for signing the rules. you can now access the server!");
+        }
     }
 });
 
