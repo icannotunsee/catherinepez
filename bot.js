b01dc69aee9a096f6ce9dbeb3a90f1389f9936e5
icannotunsee <66531908+icannotunsee@users.noreply.github.com>
2021-01-06 21:26:33 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -91,7 +91,7 @@ client.on('message', message => {
 });
 
 client.on('message', (message) => {
-  if (message.content.includes('discord.gg/'||'discordapp.com/invite/') && message.channel.id != '783175753771515904') {
+  if (message.content.includes('discord.gg/'||'discordapp.com/invite/') && !message.guild.member(message.author).hasPermission("MANAGE_CHANNELS") && message.channel.id != '783175753771515904') {
           let badMsg = message.content;
           let badMsgChan = message.guild.channels.cache.get(message.channel.id);
           let badMsgUser = message.author;
