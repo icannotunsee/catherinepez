608ca263a77e95908b3be68421edf7b8f2467418
icannotunsee <66531908+icannotunsee@users.noreply.github.com>
2021-01-06 20:13:22 -0600

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -2,11 +2,12 @@ client.on('message', message => {
   let args = message.content.substring(PREFIX.length).split(" ");
   switch (args[0]) {
     case 'embed':
-       var emb = new MessageEmbed()
-          .setColor('#FFBCC9')
-          .setDescription(message)
+      let removed = message.content.replace('~embed', '')
+      let emb = new MessageEmbed()
+         .setColor('#FFBCC9')
+         .setDescription(removed)
       
-       message.channel.send(emb);
-       message.delete();
+      message.channel.send(emb);
+      message.delete();
   }
 });
