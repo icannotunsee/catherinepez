082f3bba0b74c6394fd9a40570cbbcf406f91ef5
icannotunsee <66531908+icannotunsee@users.noreply.github.com>
2021-01-08 20:21:29 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,10 @@
 const Discord = require('discord.js');
 const { Client, MessageEmbed } = require('discord.js');
+const { Client, Intents } = require("discord.js");
 
-client = new BotClient();
+const client = new BotClient({
+  ws: { intents: Intents.ALL },
+});
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
