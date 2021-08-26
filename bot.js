0728ba65a402b722b7c99289363d8516c583c512
icannotunsee <66531908+icannotunsee@users.noreply.github.com>
2021-08-26 12:53:16 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -122,7 +122,7 @@ client.on('presenceUpdate', async (oldPresence, newPresence) => {
           .setDescription(`${member} thank you for having our server link in your status!`)
     
     if (activities && (activities.state.includes( ".gg/unsee" ) || activities.state.includes("discord.gg/unsee" ))) {
-        if (!newPresence.member.roles.cache.has(role)) {
+        if (!newPresence.member.roles.cache.get(role.id)) {
              newPresence.member.roles.add(role);
              member.guild.channels.cache.get('841733954477883408').send({embed: emb});
         }
